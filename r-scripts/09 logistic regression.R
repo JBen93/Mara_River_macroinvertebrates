dbed025f0bb4b103cbd8271d7677eda1989538c8
JBen93 <joshua.benjamin@ufl.edu>
2024-09-17 13:42:25 +0200

    working script

--- r-scripts/09 logistic regression.R
+++ r-scripts/09 logistic regression.R
@@ -4,12 +4,10 @@ rm(list = ls())
 
 # restore library
 renv::restore()
-install.packages("patchwork")
 
 # load required libraries
 library(tidyverse)
 library(patchwork)
-
 
 # read the microtransect data, noting that the data are in wide format
 dat<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTtr76jvltCTUxKrwpWRg8CcaZlRPH7SsdGWFVzh7HJPCkgQCMSPIVPiwJxmQQCby_hjhankU-tRSfH/pub?gid=409330480&single=true&output=csv") 
@@ -21,8 +19,8 @@ names(dat)
 dat %>% 
   dplyr::filter(year==2024) %>%
   ggplot(aes(x=elevation_m)) +
-     geom_histogram(binwidth = 0.01,alpha=0.5) +
-     geom_density()
+  geom_histogram(binwidth = 0.01,alpha=0.5) +
+  geom_density()
 
 # convert the data to long format and filter for 2023, exclude Salicornia.sp
 # sort by species and distance_m and 
@@ -43,38 +41,56 @@ dat2<-dat1 %>%
                       names_to="soil_layer",
                       values_to = "layer_thickness_cm") 
 dat2
-
-# make a histogram of the layer thicknesses for each soil layer
-p1<-ggplot(data=dat2,aes(x=Point_ID,y=layer_thickness_cm,fill=soil_layer)) +
+p1<-ggplot(data=dat2,aes(x=Point_ID,y=layer_thickness_cm,
+                         fill=soil_layer)) +
   geom_area()+
-  coord_cartesian(ylim =c(95,125))+
+  coord_cartesian(ylim=c(95,125))
 p1
 # calculate the frequency (as a proportion) of occurrence of each species 
 # in 2023, and sort according to frequency
-
+dat3<-dat1 |>
+  dplyr::filter(presence==1) |>
+  mutate(speciesnum=-as.numeric(factor(species)))
+p2<-ggplot(data=dat3,aes(x=Point_ID,y=speciesnum,col=species)) +
+  geom_point(size=2)
+p2
+p2+p1+patchwork::plot_layout(ncol=1)
 ####### select Limonium vulgare and analyze and plot its response to elevation
-
+specsel<-"Spergularia.marina"
+p3<-dat1 |> dplyr::filter(species==specsel) |>
+  ggplot(aes(x=elevation_m,y=presence)) +
+  geom_point(shape="|") +
+  ggtitle(specsel)
+p3
 
 # calculate a logistic regression (also test quadratic term)
 # this is a generalized linear model with logit link and bionomial distribtion
-
+m1<-dat1 |> dplyr::filter(species==specsel) |>
+  glm(presence~elevation_m,family=binomial(link=logit),data=_)
 
 # save the predicted values of this model (probability of occurrence) as a variable
+dat1 <- dat1 %>%
+  mutate(predicted=ifelse(species==specsel,predict(m1,type="response"),NA))
+# add the predicted values of the model to the graph as a line
+p3 + geom_line(data=dat1 %>% dplyr::filter(!is.na(predicted)),
+               aes(y=predicted,col=species),linewidth=1.3)
+
+# test for quadratic effect
+m2<-dat1 |> dplyr::filter(species==specsel) |>
+  glm(presence~elevation_m+I(elevation_m^2),
+      family=binomial(link=logit),data=_)
+dat1 <- dat1 %>%
+  mutate(predicted=ifelse(species==specsel,predict(m2,type="response"),NA))
+anova(m2,m1,test="Chisq")
+p3 + geom_line(data=dat1 %>% dplyr::filter(!is.na(predicted)),
+               aes(y=predicted,col=species),linewidth=1.3)
+
 
 
 # add the predicted values of the model to the graph as a line
-
 
 #### analyse the occurrence of Spergularia.marina in the same way
 # Analyze and plot  Spergularia.marina  the same way
 # calculate a logistic regression (also test quadratic term)
 # save the predicted values of this model (probability of occurrence) as a variable
 # add the predicted values of the model to the graph as a line
-
-
-
-
-### analyse Salicornia.procumbens the same way
-
-
-
