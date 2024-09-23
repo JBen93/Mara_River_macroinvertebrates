af7ccef717f44df6cbd42d92b7164708bca1d334
JBen93 <joshua.benjamin@ufl.edu>
2024-09-23 15:18:20 +0200

    current and historical differences in sites using nmds

--- r-scripts/Historical_current macros.R
+++ r-scripts/Historical_current macros.R
@@ -16,17 +16,34 @@ tibble::column_to_rownames(var="Location_ID") # convert distance_m to the row na
 
 #The NMDS plot will be used to determine the temporal changes in the macroinvertebrates community structure in the Mara river.
 
-#NMDS plot of the macros_PCoA data
-nmdsmacros<-metaMDS(combinedmacros,k=2, trace=T)
+# Calculate the Jaccard dissimilarity matrix
+jmacros <- vegdist(combinedmacros, method = "jaccard")
+
+# Perform NMDS using the Jaccard dissimilarity matrix
+nmds_jaccard <- metaMDS(jmacros, k = 2, trace =T)
+stressplot(nmds_jaccard)
+#plot out our results and see if there is a difference between the historical and current
+#Identify the time period as groups:
+treat=as.matrix(c(rep("Historical",4),rep("Current",4)))
+
+#Plot out the points (Location_ID):
+ordiplot(nmds_jaccard,type="n",xlim=c(-.5,.5),ylim=c(-.5,.5))
+
+## species scores not available
+orditorp(nmds_jaccard,display="sites",col=c(rep("green",4),rep("blue",4)),air=0.01,cex=1.25)
+legend(-.55,.5, c("Historical","Current"), cex=0.8, 
+       col=c("green","blue"), pch=15:15)
+#Add a convex hull around each group:
+
+ordihull(nmds_jaccard, treat, display="si",lty=1, col="green", show.groups="Historical")
+ordihull(nmds_jaccard, treat, display="si", lty=1, col="blue", show.groups="Current")
+
+
+
+
 
 #For plotting the NMDS, create the groups to assign different colors to each time period. You will also use these groups for the ANOSIM:
-group=as.matrix(c(rep("Historical",4),rep("Current",4)))
-ordiplot(nmdsmacros,type="n",xlim=c(-0.5,1),ylim=c(-.5,.5))
-## species scores not available for this method
-orditorp(nmdsmacros,display="sites",col=c(rep("green",4),rep("blue",4)),air=0.01,cex=1.25)
-# Add the legend on top right
-legend("topleft",legend=c("Historical","Current"),fill=c("green","blue"),bty="n",cex=0.8)
+# Adjust margins (bottom, left, top, right)
 
-# Add the title
-title("NMDS plot")
+
 
