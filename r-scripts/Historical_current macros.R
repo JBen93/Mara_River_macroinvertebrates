f5f2e075bc63350ce4fdf695209789b9208035c0
JBen93 <joshua.benjamin@ufl.edu>
2024-09-23 15:29:39 +0200

    current script

--- r-scripts/Historical_current macros.R
+++ r-scripts/Historical_current macros.R
@@ -10,13 +10,12 @@ library(psych)
 library(tidyverse)
 
 # read the vegetation data
-combinedmacros<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR9TMKMzDZtRRS5WAsC1N-8lcQyAB7FM5IInNfD7kDp-AtWM1tG57aLG2Hgq3RVrRFNE8VQq8mrqbhl/pub?gid=1573424697&single=true&output=csv") |>
-#names as variable to use
-tibble::column_to_rownames(var="Location_ID") # convert distance_m to the row names of the tibble
+combinedmacros<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR9TMKMzDZtRRS5WAsC1N-8lcQyAB7FM5IInNfD7kDp-AtWM1tG57aLG2Hgq3RVrRFNE8VQq8mrqbhl/pub?gid=1573424697&single=true&output=csv") |> 
+tibble::column_to_rownames(var="Location_ID") # #names as variable to use in nmds otherwise it will not work, only works with numbers. 
 
 #The NMDS plot will be used to determine the temporal changes in the macroinvertebrates community structure in the Mara river.
 
-# Calculate the Jaccard dissimilarity matrix
+# Calculate the Jaccard dissimilarity matrix since it's a presence/absence data
 jmacros <- vegdist(combinedmacros, method = "jaccard")
 
 # Perform NMDS using the Jaccard dissimilarity matrix
@@ -25,6 +24,9 @@ stressplot(nmds_jaccard)
 #plot out our results and see if there is a difference between the historical and current
 #Identify the time period as groups:
 treat=as.matrix(c(rep("Historical",4),rep("Current",4)))
+
+# Adjust figure margins (bottom, left, top, right)
+par(mar = c(5, 4, 4, 2) + 0.1)
 
 #Plot out the points (Location_ID):
 ordiplot(nmds_jaccard,type="n",xlim=c(-.5,.5),ylim=c(-.5,.5))
