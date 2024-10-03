d9439fbec592967739c9f017f802106b4f6237c2
JBen93 <joshua.benjamin@ufl.edu>
2024-10-03 17:57:41 -0400

    added the NMDS plot for the historical dataset

--- r-scripts/01_Historical_current macros.R
+++ r-scripts/01_Historical_current macros.R
@@ -179,10 +179,48 @@ permmacros <- adonis2(historicalmacros2 ~ Location_ID + month + year + River_rea
                       by = "margin")
 permmacros
 
-
 #the PerMANOVA test for the historical data shows that the Location_ID, month, year, and River_reach are not significant factors in explaining the variation in the macroinvertebrate community structure.
 
-#plot the 
+#plot the NMDS ordination of the historical macroinvertebrate community structure
+histomacros<-vegdist(historicalmacros2, "bray") #we use bray curtis distance matrix since it's count data
+histomacros
+
+#You are going to use the metaMDS function in the vegan package.
+#K =2 because we are interested in only two dimension (which is common for NMDS)
+#Trymax=100 because we have a small data set
+nmdshisto<-metaMDS(histomacros,k=2, trace=T, trymax=100)
+nmdshisto
+
+stressplot(nmdshisto) #plot the stress plot
+#What do the stress value and the fit (R2) of the monotonic regression tell you about the NMDS plot?
+#The stress value is  0.092  which implies fairly good fit. The stress value is a measure of how well the data are represented in the NMDS plot.
+#The closer the stress value is to 0, the better the representation.
+#The R2 value is 0.992, which is also very good. The R2 value is a measure of how well the data are represented in the NMDS plot.
+#The closer the R2 value is to 1, the better the representation.
+
+#First, we need to create a grouping variable for the historical groups. #We will use the ca package to do this. Identify the river reach, month, year and site as groups: #River reach as a grouping variable
+#First, we need to extract the site scores from the historical NMDS object.
+#We will use the scores function in the vegan package to do this.
+#We will also convert the site scores to a data frame.
+histodata.scores <- as.data.frame(scores(nmdshisto)) #Using the scores function from vegan to extract the site scores and convert to a data.frame
+histodata.scores$sites <- rownames(historicalmacros) # create a column of River reach from the original data frame macros
+histodata.scores$River_reach <-(historicalmacros$River_reach) # create a column of 
+head(histodata.scores)  #look at the data
+
+#Plot NMDS for the current macroinvertebrate community structure
+
+ggplot(histodata.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
+  geom_point() +
+  stat_ellipse(linetype="dashed") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  xlim(-2.5, 2.5)+
+  ylim(-2,3)+
+  labs(title = "2008-2009 macroinvertebrate composition")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  annotate("text", x = 2, y = 3, label = paste("Stress = ", round(nmdshisto$stress,3)))
+
+
 ####################################################################################################
 #Current macroinvertebrate taxa composition
 ####################################################################################################
@@ -233,42 +271,7 @@ data.scores$sites <- rownames(currentmacros) # create a column of River reach fr
 data.scores$River_reach <-(currentmacros$River_reach) # create a column of 
 head(data.scores)  #look at the data
 
-#Plot NMDS using ggplot2 for each grouping variable
-
-ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) +
-  geom_point() +
-  geom_text(aes(label=rownames(data.scores)),hjust=0, vjust=0)+
-  stat_ellipse() +
-  theme_bw() +
-  xlim(-2.5, 2.5)+
-  ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")
-
-#Remove the grid lines
-
-ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
-  geom_point() +
-  geom_text(aes(label=rownames(data.scores)),hjust=0, vjust=0)+
-  stat_ellipse() +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlim(-2.5, 2.5)+
-  ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")
-
-#Use dotted lines for the ellipses
-
-ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
-  geom_point() +
-  geom_text(aes(label=rownames(data.scores)),hjust=0, vjust=0)+
-  stat_ellipse(linetype="dashed") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlim(-2.5, 2.5)+
-  ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")
-
-#Replace site names with dots
+#Plot NMDS for the current macroinvertebrate community structure
 
 ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
   geom_point() +
@@ -277,29 +280,7 @@ ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   xlim(-2.5, 2.5)+
   ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")
-
-#Adjust the title to the center margin
-
-ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
-  geom_point() +
-  stat_ellipse(linetype="dashed") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlim(-2.5, 2.5)+
-  ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")+
-  theme(plot.title = element_text(hjust = 0.5))
-
-#Add stress value to the plot
-
-ggplot(data.scores, aes(x= NMDS1, y= NMDS2, col=River_reach)) + 
-  geom_point() +
-  stat_ellipse(linetype="dashed") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlim(-2.5, 2.5)+
-  ylim(-2,2)+
-  labs(title = "Macroinvertebrate composition")+
+  labs(title = "2021-2023 macroinvertebrate composition")+
   theme(plot.title = element_text(hjust = 0.5))+
   annotate("text", x = 1.5, y = 2, label = paste("Stress = ", round(nmdsmacros$stress,3)))
+
